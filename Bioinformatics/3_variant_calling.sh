5820a003e42c8dc0fc512e6b4876fa5256d9a850
KathleenMcLay <mclay.kathleen@gmail.com>
2023-11-30 14:07:23 +1000

    :sunflower:

--- Bioinformatics/3_variant_calling.sh
+++ Bioinformatics/3_variant_calling.sh
@@ -9,45 +9,45 @@ CLDIR="/g/data/ht96/McLay_UQ/inversion_paper/3_clean"
 SPDIR="/g/data/ht96/McLay_UQ/inversion_paper/4_variant_calling/split"
 VCDIR="/g/data/ht96/McLay_UQ/inversion_paper/4_variant_calling"
 
-# create an array of chromosome names to use as interval values with HaplotypeCaller
-declare -a arr=("SLv141Ch1" "SLv141Ch2" "SLv141Ch3" "SLv141Ch4" "SLv141Ch5" "SLv141Ch6" "SLv141Ch7" "SLv141Ch8" "SLv141Ch9" "SLv141Ch10" "SLv141Ch11" "SLv141Ch12" "SLv141Ch13" "SLv141Ch14" "SLv141Ch15" "SLv141Ch16" "SLv141Ch17" "SLv141Ch18" "SLv141Ch19" "SLv141Ch20")
+# create an array of scaffold names to use as interval values with HaplotypeCaller
+declare -a arr=("scaffold_1" "scaffold_2" "scaffold_3" "scaffold_4" "scaffold_5" "scaffold_6" "scaffold_7" "scaffold_8" "scaffold_9" "scaffold_10" "scaffold_11" "scaffold_12" "scaffold_13" "scaffold_14" "scaffold_15" "scaffold_16" "scaffold_17" "scaffold_18" "scaffold_19" "scaffold_20" "scaffold_21" "scaffold_22" "scaffold_23" "scaffold_24" "scaffold_25" "scaffold_26" "scaffold_27" "scaffold_28" "scaffold_29" "scaffold_30")
 
 # run HaplotypeCaller 
 for i in "${arr[@]}";
 do
-    # create a variable to isolate the chrom no. for file naming  
-    if [ ${#i} -eq 9 ]; then
-        modified_i="${i:0:8}0${i:8:1}"
+    # create a variable to isolate the scaffold no. for file naming  
+    if [ ${#i} -eq 10 ]; then
+        modified_i="${i:0:9}0${i:9:1}"
     else
         modified_i="${i}"
     fi
     chr="${modified_i: -2}"
     # run HaplotypeCaller per chromosome using -intervals
     gatk --java-options "-Xmx8g" HaplotypeCaller \
-        --input ${INDIR}/${1}_PCRduprm_cln_srt.bam \
+        --input ${CLDIR}/${1}_PCRm_cln_srt.bam \
         --output ${SPDIR}/${1}_${chr}_vrnt.g.vcf.gz \
-        --reference /scratch/ht96/km6006/SLv141Asm_Ch20RN.fasta \
+        --reference /scratch/ht96/km6006/2_D01_30.fasta \
         --emit-ref-confidence GVCF \
         --intervals $i &
 done
 
 wait
 
-# concatenate the chromosomes for each sample, remove the chromsome files once done  
+# concatenate the scaffolds for each sample
 
-# create list variables 
+#create list variables 
 file_list="${VCDIR}/${1}_file_list.txt"
 sort_list="${VCDIR}/${1}_sorted_file_list.txt"
 
 # find all files for the current sample in the output directory 
 find "$SPDIR" -type f -name "${1}_*.vcf.gz" >> "$file_list"
 
-# crete a sorted list of chromosome files and concatenate and index the files
+# print the file to the file_list variable, sort it in the sort list variable and then concatenate 
 if [ -s "$file_list" ]; then
     awk '{print $1}' "$file_list" | sort -t'_' -k5 -n > "$sort_list" 
     # Concatenate and index 
     bcftools concat --threads 6 --file-list "$sort_list" --output "${VCDIR}/${1}_vrnt.g.vcf.gz" 
-    bcftools index --threads 6 -t "${VCDIR}/${1}_vrnt.g.vcf.gz" 
+    bcftools index --threads 6 "${VCDIR}/${1}_vrnt.g.vcf.gz" 
 
 else
     echo "No match for: $1" 
