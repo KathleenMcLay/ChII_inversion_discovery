f35f7eb73b2cacdde0344e44a538be4e643943de
Kathleen McLay (qris-uq) <uqkmcla4@bunya1.rcc.uq.edu.au>
2025-02-06 16:59:15 +1000

    update PCA

--- inversion_breakpoints/flye.sh
+++ inversion_breakpoints/flye.sh
@@ -3,14 +3,14 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=48
 #SBATCH --mem=450GB
-#SBATCH --job-name=flye_T01_contigs
+#SBATCH --job-name=flye_H01_contigs
 #SBATCH --time=48:00:00
 #SBATCH --partition=general
 #SBATCH --account=a_ortiz_barrientos_coe
-#SBATCH --output=/home/uqkmcla4/scripts/flye_T01_contigs.log
+#SBATCH --output=/home/uqkmcla4/scripts/flye_H01_contigs.log
 
 IN_DIR="/QRISdata/Q6656/long_read_data/2_combined_raw_hifi"
-OUT_DIR="/QRISdata/Q6656/long_read_data/3_denovo_contigs/T01"
+OUT_DIR="/QRISdata/Q6656/long_read_data/3_denovo_contigs/H01"
 
 # Run Flye on the combined PacBio HiFi reads for contig level denovo assembly
-python /home/uqkmcla4/Flye/bin/flye --pacbio-hifi "$IN_DIR"/5_T01_combined.fastq --out-dir "$OUT_DIR" --threads 48 --asm-coverage 40 --genome-size 2.8g
+python /home/uqkmcla4/Flye/bin/flye --pacbio-hifi "$IN_DIR"/1_H01_combined.fastq.gz --out-dir "$OUT_DIR" --threads 48 --asm-coverage 40 --genome-size 2.8g
--- inversion_pop_gen/1_PCA.R
+++ inversion_pop_gen/1_PCA.R
@@ -1,34 +1,45 @@
 # kmeans clustering of all SNPs from putative inversion region to identify genotype clustering 
-
+library(cluster)  # For silhouette analysis
+library(dplyr)  
 library(tidyverse)
 library(SNPRelate)
 library(Matrix)
-library(ggrepel)
+library(ggplot2)
 
-# list of inversion regions vcfs 
-pca.files <- list.files(path = "/QRISdata/Q6656/new_lpca/inversion_vcfs", pattern=".vcf.gz", all.files=FALSE)
-sample.files <- list.files(path = "/QRISdata/Q6656/new_lpca/sample_lists", pattern=".txt", all.files=FALSE) #samples must be in same order as samples in vcf 
+inversion_list <- read.csv("/QRISdata/Q6656/chapter_II/new_inv_discovery/inversions.csv", header=TRUE)
 
-pdf("/QRISdata/Q6656/new_lpca/pca_out/PCA_plots.pdf", height=7, width=7) 
+for row in inversion_list{
+    # create a variable to filter the vcf
+    region <- row$vcf_filter
+    # create a variable for the population name/s
+    pop <- row$population
 
-for (j in 1:length(pca.files)) {
-    pop_base <- sub("\\.\\w+$", "", pca.files[j])
-    pop_base <- sub("_.+$", "", pop_base)
-    print(pop_base)
-    for (i in 1:length(sample.files)) {
-        sample_base <- sub("\\.\\w+$", "", sample.files[i])  
-        if (pop_base == sample_base) {
-        print(sample_base)
-        samples <- read.table(paste("/QRISdata/Q6656/new_lpca/sample_lists/", sample.files[i], sep=""), header=FALSE) 
-        colnames(samples) <- "sample"
-        }
+    # create the correct population prefix to create a sample list from the vcf for populations with two or more subpopulations
+    if (nchar(pop) > 2) { 
+        pop_filter <- paste0(substr(pop, 1, 3), "|", substr(pop, 4, 6))
+        } else {
+        pop_filter <- pop  # Keep original if <= 2 characters
     }
-    vcf_fn <- paste("/QRISdata/Q6656/new_lpca/inversion_vcfs/", pca.files[j], sep="")
 
-    # Convert VCF file 
-    snpgdsVCF2GDS(vcf_fn, "/QRISdata/Q6656/new_lpca/pca_out/senecio.gds") 
+    # extract a list of samples for the current population
+    x <- paste("bcftools query -l /QRISdata/Q6656/sf8_no_variants_noD1_reheader_final_nsng.vcf.gz | grep -E ^",pop_filter," > /QRISdata/Q6656/chapter_II/new_inv_discovery/",pop,"_sample_list.txt", sep = "")
+    system(x)
+
+    # filter the vcf 
+    x <- paste("bcftools view --threads 12 -O z -S /QRISdata/Q6656/chapter_II/new_inv_discovery/", pop, "_sample_list.txt -r ", region, " -o /QRISdata/Q6656/chapter_II/new_inv_discovery/inv_vcfs/", pop, "_", region, ".vcf.gz /QRISdata/Q6656/sf8_no_variants_noD1_reheader_final_nsng.vcf.gz", sep = "")
+    system(x)
+    index <- paste("bcftools index --threads 24 /QRISdata/Q6656/chapter_II/new_inv_discovery/inv_vcfs/", pop, "_", region, ".vcf.gz", sep = "")
+    system(index)
+
+    # read in list of samples for the population and keep just the list of samples
+    samples <- read.table(paste("/QRISdata/Q6656/chapter_II/new_inv_discovery/", pop, "_sample_list.txt", sep=""), header=FALSE)
+
+    # read in the vcf
+    vcf_fn <- paste("/QRISdata/Q6656/chapter_II/new_inv_discovery/inv_vcfs/", pop, "_", region, ".vcf.gz", sep="")
+    # Convert VCF file
+    snpgdsVCF2GDS(vcf_fn, paste("/QRISdata/Q6656/chapter_II/new_inv_discovery/pca/", pop, "_", region, "_senecio.gds"))
     # Assign the new file to an object 
-    genofile <- snpgdsOpen("/QRISdata/Q6656/new_lpca/pca_out/senecio.gds")
+    genofile <- snpgdsOpen(paste("/QRISdata/Q6656/chapter_II/new_inv_discovery/pca/", pop, "_", region, "_senecio.gds"))
     # Run PCA 
     senecio_pca <- snpgdsPCA(genofile, autosome.only=FALSE)
 
@@ -51,38 +62,25 @@ for (j in 1:length(pca.files)) {
     kmeans_cluster <- kmeans(PCA[,1], 3, centers=c(min(PCA[,1]), (min(PCA[,1])+max(PCA[,1]))/2, max(PCA[,1])))
     }
 
+    # Calculate silhouette score
+    sil <- silhouette(kmeans_cluster$cluster, dist(PCA[, c("PC1", "PC2")]))
+    mean_sil_score <- mean(sil[, 3])
+
     # Make genotype cluster dataset   
     cluster_genotypes <- as_tibble(cbind(samples, PCA)) %>% 
     mutate(PC1=as.double(PC1), PC2=as.double(PC2))
     cluster_genotypes$cluster <- kmeans_cluster$cluster - 1
     cluster_genotypes$cluster <- as.character(cluster_genotypes$cluster)
-    cluster_name <- paste("/QRISdata/Q6656/new_lpca/pca_out/", tools::file_path_sans_ext(pca.files[j]), "_PCA_genotypes.txt", sep= "")
+    cluster_name <- paste("/QRISdata/Q6656/chapter_II/new_inv_discovery/pca/", pop, "_", region, "_PCA_genotypes.txt", sep= "")
     write.table(cluster_genotypes, cluster_name, sep="\t")
 
     # Make percentage stats dataset 
     betweenSS_perc <- kmeans_cluster$betweenss / kmeans_cluster$totss
     stat_table <- tibble(PC1_perc=as.numeric(PC1_perc), PC2_perc=as.numeric(PC2_perc),
-                        betweenSS_perc=as.numeric(round(betweenSS_perc,4)))
-    stat_name <- paste("/QRISdata/Q6656/new_lpca/pca_out/", tools::file_path_sans_ext(pca.files[j]), "_PCA_stats.txt", sep= "")
+                        betweenSS_perc=as.numeric(round(betweenSS_perc,4)), mean_sil_score=as.numeric(round(mean_sil_score,4)))
+    stat_name <- paste("/QRISdata/Q6656/chapter_II/new_inv_discovery/pca/", pop, "_", region, "_PCA_stats.txt", sep= "")
     write.table(stat_table, stat_name, sep="\t")
 
-    # PCA Plot 
-    title <- tools::file_path_sans_ext(pca.files[j])
-    PCA_plot <- ggplot(cluster_genotypes, aes(x=PC1, y=PC2, col=cluster)) + geom_point(size = 2) + theme_bw() +
-    scale_color_manual(name="Cluster", values=c("red","purple","blue")) +
-    ggtitle(title) +
-    xlab(paste("PC1 (",PC1_perc, "%)")) +
-    ylab(paste("PC2 (",PC2_perc, "%)")) +
-    theme(legend.title = element_text(size=18, face="bold"), 
-          legend.text=element_text(size=15),
-          plot.margin = unit(c(0.3,0.3,0.3,0.3), "inches"), 
-          axis.title.x = element_text(margin = margin(t = 20), face="bold", size = 12), 
-          axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0), face="bold", size = 12),
-          plot.title = element_text(vjust = 4, size = 10), axis.text.x=element_text(colour="black"), axis.text.y=element_text(colour="black"),
-          axis.text = element_text(size = 12),
-          legend.position = "none")    
-    print(PCA_plot)
     snpgdsClose(genofile)
-    file.remove("/QRISdata/Q6656/new_lpca/pca_out/senecio.gds")
 }
 dev.off()
