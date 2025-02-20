c97a798b7716bfc75f9f4c481558e603e95d2f27
KathleenMcLay <81064774+KathleenMcLay@users.noreply.github.com>
2025-02-20 13:59:52 +1000

    Update Fst_LD.sh

--- inversion_pop_gen/Fst_LD.sh
+++ inversion_pop_gen/Fst_LD.sh
@@ -1,32 +1,38 @@
 dir="/QRISdata/Q6656/chapter_II/new_inv_discovery/alt"
 gzvcf="/QRISdata/Q6656/sf8_no_variants_noD1_reheader_final_nsng.vcf.gz"
 
-inversions="/QRISdata/Q6656/chapter_II/new_inv_discovery/alt/4_regions_results_summary.csv"
+inversions="/QRISdata/Q6656/chapter_II/new_inv_discovery/alt/inversions.csv"
 
 while IFS="," read -r inv population scaffold; do
-    echo "current inversion is ${inv}"
+    echo "current inversion is ${inv}, ${population}, ${scaffold}"
 
-    # check if the files already exist
-    if [ -f "${dir}/pop_vcfs/${inv}_${population}.vcf.gz" ]; then
-        echo "vcf file already exists"
-        continue
-    fi
+    # # check if the files already exist
+    # if [ -f "${dir}/pop_vcfs/${inv}_${population}.vcf.gz" ]; then
+    #     echo "vcf file already exists"
+    #     continue
+    # fi
+    # echo "${scaffold}."
+    # # filter the vcf to the current population and scaffold 
+    # scaffold=$(echo ${scaffold} | tr -d '\n\r')
+    # bcftools view --threads 12 -O z \
+    #     -S "${dir}/${population}_sample_list.txt" \
+    #     -r "${scaffold}" \
+    #     --output "${dir}/pop_vcfs/${inv}_${population}.vcf.gz" \
+    #     "${gzvcf}"
 
-    # filter the vcf to the current population and scaffold 
-    bcftools view --threads 12 -O z --regions ${scaffold} -S ${dir}/${population}_sample_list.txt --output ${dir}/pop_vcfs/${inv}_${population}.vcf.gz ${gzvcf}
-    echo "filtered vcf created"
-    # index the filtered VCF
-    bcftools index --threads 24 ${dir}/pop_vcfs/${inv}_${population}.vcf.gz
-    echo "vcf indexed"
+    # echo "filtered vcf created"
+
+    # # index the filtered VCF
+    # bcftools index --threads 12 ${dir}/pop_vcfs/${inv}_${population}.vcf.gz
+
+    # echo "vcf indexed"
+    
     # create files for genotype lists
     G0="${dir}/${inv}_${population}_G0.txt"
     G2="${dir}/${inv}_${population}_G2.txt"
 
-    # read in the sample genotype data for all inversions
-    genotypes="${dir}/3_PCA_het_all_inversions_final.csv"
-    awk -F',' '{print $1","$2","$3","$4}' "$genotypes" > "${dir}/${inv}_${population}_genotypes.csv"
-    genotypes="${dir}/${inv}_${population}_genotypes.csv"
-    head "$genotypes"
+    # read in the sample genotype data for all inversions 
+    genotypes="${dir}/genotypes.csv"
 
     # For the current inversion add each homozygous sample to the correct list, based on their genotype
     while IFS="," read -r pop sample inversion genotype; do
@@ -52,9 +58,19 @@ while IFS="," read -r inv population scaffold; do
         --out "${dir}/fst/${inv}_${population}"
 
     # Calculate LD for all samples in the inversion
-    vcftools --gzvcf ${dir}/pop_vcfs/${inv}_${population}.vcf.gz --geno-r2 --maf 0.05 --thin 1000 --max-missing-count 0 --out ${dir}/ld/${inv}_${population}
+    if [ ! -f "${dir}/ld/${inv}_${population}.geno.ld" ]; then
+        vcftools --gzvcf ${dir}/pop_vcfs/${inv}_${population}.vcf.gz --geno-r2 --maf 0.05 --thin 1000 --max-missing-count 0 --out ${dir}/ld/${inv}_${population}
+    else
+        echo "LD file already exists"
+    fi
 
     # subset the vcf file to just the samples from each homozygous genotype group
+    # check if the file exists already 
+    if [ -f "${dir}/ld/${inv}_${population}_G0.vcf.gz" ]; then
+        echo "vcf file already exists"
+        continue
+    fi
+
     bcftools view --threads 12 --samples-file ${G0} -O z -o ${dir}/ld/${inv}_${population}_G0.vcf.gz ${dir}/pop_vcfs/${inv}_${population}.vcf.gz
     bcftools index -t ${dir}/ld/${inv}_${population}_G0.vcf.gz
 
@@ -69,3 +85,4 @@ while IFS="," read -r inv population scaffold; do
     cat ${dir}/ld/${inv}_${population}_G2.geno.ld | perl /home/uqkmcla4/scripts/reformat/emerald2windowldcounts.pl > ${dir}/ld/${inv}_${population}_G2_ld.txt
 
 done < "$inversions"
+
